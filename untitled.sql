3fb3f84c10a190f8e69578dc7a56d058150c5b03
Ryrico <145660524+RyricoD321221@users.noreply.github.com>
2023-11-30 18:55:03 +0300

    Add files via upload

---
+++ untitled.sql
@@ -0,0 +1,32 @@
+ALTER TABLE `Студенты` DROP FOREIGN KEY `Студенты_fk0`;
+
+ALTER TABLE `Продажа` DROP FOREIGN KEY `Продажа_fk0`;
+
+ALTER TABLE `Продажа` DROP FOREIGN KEY `Продажа_fk1`;
+
+ALTER TABLE `Способ` DROP FOREIGN KEY `Способ_fk0`;
+
+ALTER TABLE `Группы` DROP FOREIGN KEY `Группы_fk0`;
+
+ALTER TABLE `Преподаватели` DROP FOREIGN KEY `Преподаватели_fk0`;
+
+ALTER TABLE `Преподаватели` DROP FOREIGN KEY `Преподаватели_fk1`;
+
+ALTER TABLE `Преподаватели` DROP FOREIGN KEY `Преподаватели_fk2`;
+
+ALTER TABLE `Формат` DROP FOREIGN KEY `Формат_fk0`;
+
+ALTER TABLE `Формат` DROP FOREIGN KEY `Формат_fk1`;
+
+ALTER TABLE `Языки` DROP FOREIGN KEY `Языки_fk0`;
+
+ALTER TABLE `Направления` DROP FOREIGN KEY `Направления_fk0`;
+
+ALTER TABLE `Время` DROP FOREIGN KEY `Время_fk0`;
+
+ALTER TABLE `Курсы` DROP FOREIGN KEY `Курсы_fk0`;
+
+ALTER TABLE `Курсы` DROP FOREIGN KEY `Курсы_fk1`;
+
+ALTER TABLE `Уровни` DROP FOREIGN KEY `Уровни_fk0`;
+
